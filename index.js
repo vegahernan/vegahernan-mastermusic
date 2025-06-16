3f22d199196626276f5989621f1f09dac3541357
vegahernan <vegahg@gmail.com>
2025-06-16 16:45:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+export default function Home() {
+  return (
+    <div style={{
+      backgroundColor: '#000',
+      color: '#fff',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif',
+      textAlign: 'center',
+      padding: '0 1rem'
+    }}>
+      <h1>Sabés lo que te gusta.<br />Descubrí lo que todavía no sabías.</h1>
+    </div>
+  );
+}
